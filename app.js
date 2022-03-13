680628ba035ac11b3429de43abdf6c3658d68316
Alexander Larin <doremilarin@gmail.com>
2022-03-13 21:52:26 +0300

    added func of hiding and showing the display of elements

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
 "use strict";
 
-let title = document.querySelector('.title');
-let btn = document.querySelector('.button');
+let hiddenBtn = document.querySelector('.hidden__element'),
+    title = document.querySelector('.title'),
+    changeBtn = document.querySelector('#change-btn'),
+    hideBtn = document.querySelector('#hide-btn');
 
 
-btn.addEventListener('click', () => {
-    const value = getRandom();
+changeBtn.addEventListener('click', () => {
+    const value = getRandomColor();
+    const randomColor = `rgb(${value[0]}, ${value[1]}, ${value[2]})`;
+    
+    title.textContent = randomColor;
+    document.body.style.background = randomColor;
 
-    title.textContent = `RGB(${value[0]}, ${value[1]}, ${value[2]})`;
-    btn.style.background = `rgb(${value[0]}, ${value[1]}, ${value[2]})`;
-    document.body.style.background = `rgb(${value[0]}, ${value[1]}, ${value[2]})`;
+    changeBtn.style.background = randomColor;
+    hideBtn.style.background = randomColor;
 });
 
 
-btn.addEventListener('click', () => {
-    btn.style.borderColor = 'white';
-    btn.style.color = 'white';
+changeBtn.addEventListener('click', () => {
+    changeBtn.style.borderColor = 'white';
+    changeBtn.style.color = 'white';
+    hideBtn.style.borderColor = 'white';
+    hideBtn.style.color = 'white';
 },  { once: true });
 
 
-function getRandom() {
+hideBtn.addEventListener('click', hideDisplay); 
+
+hiddenBtn.addEventListener('click', showDisplay); 
+
+
+function hideDisplay() {
+    title.classList.add('opacity');
+    changeBtn.classList.add('opacity');
+    hideBtn.classList.add('opacity');
+}
+
+function showDisplay() {
+    title.classList.remove('opacity');
+    changeBtn.classList.remove('opacity');
+    hideBtn.classList.remove('opacity');
+}
+
+function getRandomColor() {
     const num = [];
 
     for (let i = 0; i < 3; i++) {
